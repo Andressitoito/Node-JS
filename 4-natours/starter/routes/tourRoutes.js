9a921184d538bcff781f1d83cecefac93531a408
Andressitoito <andresledesma87@gmail.com>
2022-07-22 19:02:55 -0300

    update

---
+++ 4-natours/starter/routes/tourRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const tourController = require('../controllers/tourController')
+const { getAllTours, createTour, getTour, updateTour, deleteTour } = tourController
+const router = express.Router()
+
+// app.get('/api/v1/tours', getAllTours)
+// app.get('/api/v1/tours/:id', getTour)
+// app.post('/api/v1/tours', createTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour)
+
+router.param('id', tourController.checkId)
+
+router.route('/')
+ .get(getAllTours)
+ .post(tourController.checkBody, createTour)
+
+router.route('/:id')
+ .get(getTour)
+ .patch(updateTour)
+ .delete(deleteTour)
+
+module.exports = router
