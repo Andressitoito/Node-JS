7ac2137ed3a47b9e7c6d6b0a4a7749c01a208859
Andressitoito <andresledesma87@gmail.com>
2022-07-25 18:34:47 -0300

    Update

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,26 +1,25 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // 1 MIDDLEWARES
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
- app.use(morgan('dev'))
+  app.use(morgan('dev'));
 }
-app.use(express.json())
+app.use(express.json());
 
-console.log('hiny')
-
+console.log('hiny');
 
 app.use((req, res, next) => {
- console.log('Hello from the middleware')
- /* NEVER FORGET NEXT */
- next()
-})
+  console.log('Hello from the middleware');
+  /* NEVER FORGET NEXT */
+  next();
+});
 
 /* 
 
@@ -29,26 +28,19 @@ app.use((req, res, next) => {
 
 */
 app.use((req, res, next) => {
- req.requestTime = new Date().toISOString()
- next()
-})
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 // 2 ROUTE HANDLERS
 
 // 3 ROUTES
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // 4 START SERVER
 
-
-module.exports = app
-
-
-
-
-
-
+module.exports = app;
 
 /* 
 
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,12 +1,9 @@
-const dotenv = require('dotenv')
-dotenv.config({ path: './config.env' })
-const app = require('./app')
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
- console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
-
-const x = 4
-x = 34
-
